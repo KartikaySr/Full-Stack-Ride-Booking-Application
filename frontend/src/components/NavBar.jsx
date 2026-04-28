9b84fdef084bde757a4dfbbe04c6feb5b0163600
Kartikay Srivastava <kartikaymg57@gmail.com>
2026-04-29 00:45:23 +0530

    Add files via upload

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+// src/components/Navbar.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="logo">
+          Uber
+        </Link>
+        <Link to="/rider">Ride</Link>
+        <Link to="/driver">Drive</Link>
+        <Link to="/business">Business</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <div className="navbar-right">
+        <Link to="/help">Help</Link>
+        <Link to="/login">Log in</Link>
+        <Link to="/register" className="signup-btn">
+          Sign up
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
